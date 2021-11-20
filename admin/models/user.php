c627e3ae73aa6230b299e9b587566d25fddcf27e
Zahrandz <93361401+Zahrandz@users.noreply.github.com>
2021-11-20 10:48:06 +0700

    Update user.php

--- admin/models/user.php
+++ admin/models/user.php
@@ -9,22 +9,23 @@ class user{
         $this->db = $this->db->get_koneksi();
     }
  
-    // public function tambah($penerbit, $alamat)
-    // {
-    //     $insert = $this->db->prepare('INSERT INTO `penerbit` (`penerbit`,`alamat`) VALUES (?, ?)');
-    //     $insert->bindParam(1, $penerbit);
-    //     $insert->bindParam(2, $alamat);
-    //     $insert->execute();
-    //     return $insert;
-    // }
+     public function tambah($nama, $username, $level)
+    {
+        $insert = $this->db->prepare('INSERT INTO `user` (`nama`,`username`,`level`) VALUES (?, ?, ?)');
+        $insert->bindParam(1, $nama);
+        $insert->bindParam(2, $username);
+        $insert->bindParam(3, $level);
+        $insert->execute();
+        return $insert;
+    }
  
-    // public function tampil()
-    // {
-    //     $show = $this->db->prepare("SELECT `id_penerbit`,`penerbit`,`alamat` FROM `penerbit` ORDER BY `penerbit`");
-    //     $show->execute();
-    //     $data = $show->fetchAll();
-    //     return $data;
-    // }
+    public function tampil()
+    {
+        $show = $this->db->prepare("SELECT `id_user`,`nama`,`username`,`level` FROM `user` ORDER BY `user`");
+        $show->execute();
+        $data = $show->fetchAll();
+        return $data;
+    }
  
     public function tampil_id($id_user){
         $show = $this->db->prepare("SELECT * FROM user WHERE id_user = ?");
@@ -41,21 +42,22 @@ class user{
         return $show->fetch();
     }
  
-    // public function ubah($id_penerbit,$penerbit,$alamat){
-    //     $update = $this->db->prepare('UPDATE `penerbit` set `penerbit`=?,`alamat`=? where `id_penerbit`=?');
-    //     $update->bindParam(1, $penerbit);
-    //     $update->bindParam(2, $alamat);
-    //     $update->bindParam(3, $id_penerbit);
-    //     $update->execute();
-    //     return $update;
-    // }
+    public function ubah($id_user,$nama,$username, $level){
+        $update = $this->db->prepare('UPDATE `user` set `nama`=?,`username`=?, `level`=? where `id_user`=?');
+        $update->bindParam(1, $nama);
+        $update->bindParam(2, $username);
+        $update->bindParam(3, $level);
+        $update->bindParam(3, $id_user);
+        $update->execute();
+        return $update;
+    }
  
-    // public function hapus($id_penerbit)
-    // {
-    //     $delete = $this->db->prepare("DELETE FROM `penerbit` where `id_penerbit`=?"); 
-    //     $delete->bindParam(1, $id_penerbit); 
-    //     $delete->execute();
-    //     return $delete;
-    // }
+    public function hapus($id_user)
+    {
+        $delete = $this->db->prepare("DELETE FROM `user` where `id_user`=?"); 
+        $delete->bindParam(1, $id_user); 
+        $delete->execute();
+        return $delete;
+    }
 }
 ?>
