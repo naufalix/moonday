4c621c8eb0e15042da4542582d98a072ac82df92
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2021-11-22 01:18:02 +0700

    Load datatables

--- admin/views/includes/head.php
+++ admin/views/includes/head.php
@@ -12,13 +12,13 @@
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icon2.png">
 
     <!-- Custom CSS -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
 
     <!-- JS Libraries -->
     <script src="assets/js/sweetalert.min.js"></script>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+    <link rel="stylesheet" href="assets/node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="assets/node_modules/datatables.net-bs4/css/responsive.dataTables.min.css">
+
+    <style type="text/css">
+        td {vertical-align: middle !important; padding: 12px !important;}
+    </style>
--- admin/views/includes/script.php
+++ admin/views/includes/script.php
@@ -1,15 +1,24 @@
-<script src="assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="assets/node_modules/popper/popper.min.js"></script>
-    <script src="assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
     <script src="assets/js/perfect-scrollbar.jquery.min.js"></script>
     <!--Wave Effects -->
     <script src="assets/js/waves.js"></script>
     <!--Menu sidebar -->
     <script src="assets/js/sidebarmenu.js"></script>
-    <!--stickey kit -->
-    <script src="assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="assets/node_modules/sparkline/jquery.sparkline.min.js"></script>
     <!--Custom JavaScript -->
     <script src="assets/js/custom.min.js"></script>
+    <!--Library JavaScript -->
+    <script src="assets/node_modules/popper/popper.min.js"></script>
+    <script src="assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="assets/node_modules/sparkline/jquery.sparkline.min.js"></script>
+    <script src="assets/node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="assets/node_modules/datatables.net-bs4/js/dataTables.responsive.min.js"></script>
+    <script>
+    $(document).ready(function () {
+      $('#myTable').DataTable({
+        "aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All Pages"]],
+        "pageLength": 10
+      });
+    });
+    </script>
