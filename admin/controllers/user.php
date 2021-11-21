400b06c10b4548c345f725a4df6c6c717ac2aef3
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2021-11-22 01:18:33 +0700

    CRUD User

--- admin/controllers/user.php
+++ admin/controllers/user.php
@@ -1,3 +1,81 @@
 <?php
+	//require("models/user.php");
+  $user = new user();
+  
+  /* Tambah */ 
+  if (isset($_POST["submit-tambah"])) {
+    if (!empty($_POST["nama"])&&!empty($_POST["username"])&&!empty($_POST["password"])&&!empty($_POST["level"])) {
+      $n = $_POST["nama"];
+      $u = $_POST["username"];
+      $p = MD5($_POST["password"]);
+      $l = $_POST["level"];
+      if (!($user->cek_username($u))) {
+        $user->tambah($n,$u,$p,$l);
+        $success = "Akun berhasil ditambahkan";
+      } else {$error = "Username telah terpakai";}
+    }else {$error = "Semua data wajib diisi!";}
+  }
 
+  /* Edit */
+  if (isset($_POST["submit-edit"])) {
+    if (!empty($_POST["id_user"])&&!empty($_POST["nama"])&&!empty($_POST["username"])&&!empty($_POST["level"])) {
+      $i = $_POST["id_user"];
+      $n = $_POST["nama"];
+      $u = $_POST["username"];
+      $l = $_POST["level"];
+      $ul = $user->tampil_id($i)["username"];
+      if ($u==$ul) {
+        $user->ubah($i,$n,$u,$l);
+        if (!empty($_POST["p"])) {
+          $p = MD5($_POST["p"]);
+          $user->password($i,$p);
+        }
+        $success = "Akun berhasil diedit";
+      }
+      if ($u!=$ul) {
+        if (!($user->cek_username($u))) {
+          $user->ubah($i,$n,$u,$l);
+	        if (!empty($_POST["p"])) {
+	          $p = MD5($_POST["p"]);
+	          $user->password($i,$p);
+	        }
+          $success = "Akun berhasil di edit";
+        } else {$error = "Username ".$u." telah terpakai";}
+      }
+    } 
+  }
+
+  /* Edit Foto*/ 
+  if (isset($_POST["submit-foto"])) {
+    if (!empty($_POST['id_user'])&&!empty($_FILES['foto'])) {
+      $i       = $_POST["id_user"];
+      $f_tmp   = $_FILES['foto']['tmp_name'];
+      $f_name  = $_FILES['foto']['name']; 
+      $f_exp   = explode('.',$f_name);
+      $f_ext   = end($f_exp);
+      $n       = $i.".png";
+      $dir     = 'assets/images/users/'.$n;
+      if(move_uploaded_file($f_tmp,$dir)){ 
+        $user->foto($i,$n);
+        $success="Foto berhasil diupload";
+      }else{
+        $error="Foto gagal diupload";
+      }
+    }
+  }
+
+  /* Hapus User*/ 
+  if (isset($_POST["submit-hapus"])) {
+    if (!empty($_POST["id_user"])) {
+      $i = $_POST["id_user"];
+      //get foto
+      $f = $user->tampil_id($i)["foto"];
+      $n = $user->tampil_id($i)["nama"];
+      unlink("assets/images/users/$f");
+      $user->hapus($i);
+      $success = $n." berhasil dihapus";
+    }
+  } 
+
+  $data_user = $user->tampil();
 ?>
--- admin/views/user.php
+++ admin/views/user.php
@@ -18,74 +18,223 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
-
-                            <div class="col-sm-12">
-                                <div class="alert alert-success" role="alert">Data berhasil ditambahkan</div>
-                            </div>
-
                                 <h4 class="card-title float-left" style="margin:20px;">Daftar User</h4>
                                 <div class="form-group">
                                     <div class="col-sm-12">
-                                        <a href="index.php?page=tambahuser">
-                                        <button class="btn btn-danger float-right" style="margin-bottom:30px;">
-                                        <i class="fa fa-plus"></i> Tambah User</button></a>
+                                        <button class="btn btn-info float-right" data-toggle="modal" data-target="#tambah" style="margin-bottom:30px;">
+                                            <i class="fa fa-plus-circle"></i> Tambah User
+                                        </button>
                                     </div>
                                 </div>
                                 <div class="table-responsive">
-                                    <table class="table">
+                                    <table id="myTable" class="table table-responsive table-bordered table-striped">
                                         <thead>
                                             <tr>
-                                                <th>No</th>
-                                                <th>Nama</th>
+                                                <th class="text-center">No</th>
+                                                <th style="width: 100%;">Nama</th>
                                                 <th>Username</th>
                                                 <th>Role</th>
-                                                <th><center>Aksi</center></th>
+                                                <th style="min-width: 110px;"><center>Aksi</center></th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            <?php
+                                                $no=1;
+                                                foreach($data_user as $row) {
+                                                    $id_user    = $row['id_user'];
+                                                    $nama       = $row['nama'];
+                                                    $username   = $row['username'];
+                                                    $level      = $row['level'];
+                                                    $foto       = $row['foto'];
+                                                    if (empty($foto)) {$foto="default.png";}
+                                            ?>
                                             <tr>
-                                                <td>1</td>
-                                                <td>James</td>
-                                                <td>james_1</td>
-                                                <td>admin</td>
+                                                <td class="text-center"><?= $no ?></td>
+                                               <!--  <td><?= $nama ?></td> -->
+                                                <td>
+                                                    <img src="assets/images/users/<?= $foto ?>" alt="user" height="30" width="30" class="rounded-circle mr-2" style="object-fit: cover">
+                                                    <?= $nama ?>
+                                                </td>
+                                                <td><?= $username ?></td>
+                                                <td><span class="label label-success"><?= $level ?></span></td>
                                                 <td align="center">
-                                                <a href="index.php?page=edituser" class="btn btn-s btn-info" title="Edit"><i class="ti-pencil-alt"></i></a>
-                                                <a href="index.php?page=detailuser" class="btn btn-s btn-info" title="Detail"><i class="ti-eye"></i></a>
-                                                <a href="javascript:if(confirm('Anda yakin ingin menghapus data?')) 
-                                                window.location.href =''"
-                                                class="btn btn-s btn-warning"><i class="ti-trash"
-                                                title="Hapus"></i></a>
+                                                    <button type="button" class="btn btn-s btn-info" title="Edit" data-toggle="modal" data-target="#edit" onclick="edit(<?= $id_user ?>)"><i class="fa fa-pencil"></i></button>
+                                                    <p id="<?= $id_user ?>" class="d-none"><?php echo $nama.'|'.$username.'|'.$level ?></p>
+                                                    <button type="button" class="btn btn-s btn-info" title="Edit foto" data-toggle="modal" data-target="#foto" onclick="foto(<?= $id_user ?>)"><i class="fa fa-camera"></i></button>
+                                                    <button type="button" class="btn btn-s btn-danger" title="Hapus" data-toggle="modal" data-target="#hapus" onclick="hapus(<?= $id_user ?>)"><i class="fa fa-trash"></i></button>
                                                 </td>
                                             </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>Mary</td>
-                                                <td>Mary21</td>
-                                                <td>superadmin</td>
-                                                <td align="center">
-                                                <a href="index.php?page=edituser" class="btn btn-s btn-info" title="Edit"><i class="ti-pencil-alt"></i></a>
-                                                <a href="index.php?page=detailuser" class="btn btn-s btn-info" title="Detail"><i class="ti-eye"></i></a>
-                                                <a href="javascript:if(confirm('Anda yakin ingin menghapus data?')) 
-                                                window.location.href =''"
-                                                class="btn btn-s btn-warning"><i class="ti-trash"
-                                                title="Hapus"></i></a>
-                                                </td>
-                                            </tr>
+                                            <?php $no++; } ?>
                                         </tbody>
                                     </table>
-                                    <!-- /.card-body -->
-                                    <div class="card-footer clearfix">
-                                        <ul class="pagination pagination-sm m-0 float-right">
-                                        <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div id="tambah" class="modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Form tambah user</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="forms-sample" method="post">
+                            <div class="form-group">
+                              <div class="row m-0">
+                                <div class="col-7 p-0 pr-2">
+                                  <label for="nama">Nama</label>
+                                  <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama..." required>
+                                </div>
+                                <div class="col-5 p-0 pr-2">
+                                  <label for="username">Username</label>
+                                  <input type="text" class="form-control" id="username" name="username" placeholder="Username..." required>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <div class="row m-0">
+                                <div class="col-6 p-0 pr-2">
+                                  <label for="password">Password</label>
+                                  <input type="password" class="form-control" id="password" name="password" placeholder="Password..." minlength="5" required>
+                                </div>
+                                <div class="col-6 p-0 pr-2">
+                                  <label for="level">Level</label>
+                                  <select class="form-control" id="level" name="level">
+                                    <option value="admin">Admin</option>
+                                    <option value="superadmin">Superadmin</option>
+                                  </select>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                              <button type="submit" class="btn btn-success" name="submit-tambah" ><i class="fa fa-check"></i><span> Submit</span></button>
+                            </div>
+                          </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="edit" class="modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Form edit user</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="forms-sample" method="post">
+                                <input type="hidden" class="d-none" id="ei" name="id_user">
+                                <div class="form-group">
+                                  <div class="row m-0">
+                                    <div class="col-7 p-0 pr-2">
+                                      <label for="nama">Nama</label>
+                                      <input type="text" class="form-control" id="en" name="nama" placeholder="Nama..." required>
+                                    </div>
+                                    <div class="col-5 p-0 pr-2">
+                                      <label for="username">Username</label>
+                                      <input type="text" class="form-control" id="eu" name="username" placeholder="Username..." required>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="form-group">
+                                  <div class="row m-0">
+                                    <div class="col-6 p-0 pr-2">
+                                      <label for="password">Password</label>
+                                      <input type="password" class="form-control" id="ep" name="password">
+                                      <label class="text-danger">*kosongkan jika tidak ingin mengubah password</label>
+                                    </div>
+                                    <div class="col-6 p-0 pr-2">
+                                      <label for="level">Level</label>
+                                      <select class="form-control" id="el" name="level">
+                                        <option class="el" value="admin">Admin</option>
+                                        <option class="el" value="superadmin">Superadmin</option>
+                                      </select>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                  <button type="submit" class="btn btn-success" name="submit-edit"><i class="fa fa-save"></i><span> Save</span></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="foto" class="modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="fd">Form edit foto</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="forms-sample" method="post" enctype="multipart/form-data">
+                              <div class="form-group">
+                                <input type="hidden" class="d-none" id="fi" name="id_user" required>
+                                <p for="foto">Upload foto :</p>
+                                <input type="file" class="form-control" name="foto" required style="height: auto">
+                              </div>
+                              <div class="modal-footer p-0 pt-3">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-success btn-shadow" name="submit-foto"><i class="fa fa-save"></i><span> Simpan</span></button>
+                              </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="hapus" class="modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Hapus user</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="forms-sample" method="post">
+                              <div class="form-group">
+                                <p id="ht">Apakah anda yakin ingin menghapus user ini?</p>
+                                <input type="hidden" class="d-none" class="form-control" id="hi" name="id_user" value="" required>
+                              </div>
+                              <div class="modal-footer p-0 pt-3">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-danger btn-shadow" name="submit-hapus"><i class="fa fa-times"></i><span> Hapus</span></button>
+                              </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <script type="text/javascript">
+              function edit(id){
+                var data = (document.getElementById(id).textContent).split("|");
+                document.getElementById("ei").value = id;
+                document.getElementById("en").value = data[0];
+                document.getElementById("eu").value = data[1];
+                for (var i = 0; i < document.getElementsByClassName("el").length ; i++) {
+                  if (document.getElementsByClassName("el")[i].value==data[2]) {
+                    document.getElementsByClassName("el")[i].selected = "true";
+                  }
+                }
+              }
+              function foto(id){
+                var data = (document.getElementById(id).textContent).split("|");
+                document.getElementById("fi").value = id;
+                document.getElementById("fd").textContent = 'Edit foto '+data[0];
+              }
+              function hapus(id){
+                var data = (document.getElementById(id).textContent).split("|");
+                document.getElementById("hi").value = id;
+                document.getElementById("ht").textContent = 'Apakah anda yakin ingin menghapus "'+data[0]+'"?';
+              }
+            </script>
