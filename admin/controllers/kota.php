2fe060cda52497708a5702b140925e132d1dab42
luthfiyahisn <94186079+luthfiyahisn@users.noreply.github.com>
2021-11-23 18:19:39 +0700

    Update kota.php

--- admin/controllers/kota.php
+++ admin/controllers/kota.php
@@ -1,3 +1,35 @@
 <?php
+    require("models/kota.php");
+    $kota = new kota();
+    
+    /* Tambah Data */
+    if (isset($_POST["submit-tambah"])) {
+        if (!empty($_POST["kota"])) {
+        $dataKota = $_POST["kota"];
+        $kota->tambah($dataKota);
+        $success = "kota berhasil ditambahkan";
+        }else {$error = "Data kota wajib diisi!";}
+    }
 
+    /* Edit Data */
+    if (isset($_POST["submit-edit"])) {
+        if (!empty($_POST["id_kota"])&&!empty($_POST["kota"])) {
+        $idKota = $_POST["id_kota"];
+        $dataKota = $_POST["kota"];
+        $kota->edit($dataKota, $idKota);
+        $success = "Data berhasil diedit";
+        }
+    }
+
+    /* Hapus Data*/
+    if (isset($_POST["submit-hapus"])) {
+        if (!empty($_POST["id_kota"])) {
+        $idKota = $_POST["id_kota"];
+        $kota->hapus($idKota);
+        $success = "Data kota berhasil dihapus";
+        }
+    }
+    
+    /* Tampil Data */
+    $dataKota = $kota->tampil();
 ?>
