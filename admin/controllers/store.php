dc3a4a27900038a81fad3c1994efd82e9dfe9f93
luthfiyahisn <94186079+luthfiyahisn@users.noreply.github.com>
2021-11-27 20:15:09 +0700

    Create store.php

---
+++ admin/controllers/store.php
@@ -0,0 +1,43 @@
+<?php
+    require("models/store.php");
+    require("models/kota.php");
+    $store = new store();
+    $kota = new kota();
+    
+    /* Tambah */ 
+    if (isset($_POST["submit-tambah"])) {
+      if (!empty($_POST["nama"]) && !empty($_POST["id_kota"]) && !empty($_POST["lokasi"])) {
+        $n = $_POST["nama"];
+        $k = $_POST["id_kota"];
+        $l = $_POST["lokasi"];
+        $store->tambah($n,$k,$l);
+        $success = "Store berhasil ditambahkan";
+        }else {
+            $error = "Data Store wajib diisi!";
+        }
+    }
+  
+    /* Edit */
+    if (isset($_POST["submit-edit"])) {
+      if (!empty($_POST["id_store"]) && !empty($_POST["nama"]) && !empty($_POST["id_kota"]) && !empty($_POST["lokasi"])) {
+        $i = $_POST["id_store"];
+        $n = $_POST["nama"];
+        $k = $_POST["id_kota"];
+        $l = $_POST["lokasi"];
+          $store->ubah($i,$n,$k,$l);
+          $success = "Data berhasil diedit";
+      } 
+    }
+  
+    /* Hapus Store*/ 
+    if (isset($_POST["submit-hapus"])) {
+      if (!empty($_POST["id_store"])) {
+        $i = $_POST["id_store"];
+        $store->hapus($i);
+        $success = " Data Store berhasil dihapus";
+      }
+    } 
+  
+    $data_store = $store->tampil();
+    $data_kota = $kota->tampil();
+?>
