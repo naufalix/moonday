8003d7fba87f783ba48f27353c26b2fd3e092429
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2021-12-01 01:13:25 +0700

    Add index.php & config views user

---
+++ config/views-user.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("views/includes/head.php")?>
+</head>
+<body class="animsition">
+    
+    <!-- Header -->
+    <header>
+        <?php include("views/includes/header.php")?>
+    </header>
+        
+    <?php include("views/$page.php"); ?>
+
+    <!-- Footer -->
+    <footer class="bg3  p-b-32">
+        <?php include("views/includes/footer.php")?>
+    </footer>
+
+    <!-- Back to top -->
+    <div class="btn-back-to-top" id="myBtn">
+        <span class="symbol-btn-back-to-top">
+            <i class="zmdi zmdi-chevron-up"></i>
+        </span>
+    </div>
+
+    <!-- View Detail -->
+    <?php include("views/includes/detail.php")?>
+    
+    <?php include("views/includes/script.php")?>
+</body>
+</html>
